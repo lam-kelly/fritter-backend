391bde809e0e6e846120973f809171fc36539b9c
Kelly Lam <lamk@mit.edu>
2022-11-01 16:10:58 -0400

    Fix typo with removing edit-freet from frontend

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -37,7 +37,7 @@ const formsAndHandlers = {
   'view-all-freets': viewAllFreets,
   'view-freets-by-author': viewFreetsByAuthor,
   'create-freet': createFreet,
-  'edit-freet': editFreet,
+  // 'edit-freet': editFreet,
   'delete-freet': deleteFreet,
   'follow-user': followUser,
   'view-all-followers': viewAllFollowers,
